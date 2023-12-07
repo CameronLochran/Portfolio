4ee700edb6d01007a60f16195bc12afc37c9118b
WeeCameron04 <clochran04@gmail.com>
2023-12-07 14:33:02 +0000

    css started for projects page

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -1,56 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import React from 'react'
+import './ProjectsPage.css';
 
 const ProjectsPage = () => {
 
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  // const [isMoreExpanded, setIsMoreExpanded] = useState(false);
-
-  const toRotate = [
-    "My Projects",
-  ];
-  const [text, setText] = useState("");
-  const [delta, setDelta] = useState(300 - Math.random() * 500);
-  const period = 2000;
-
-  useEffect(() => {
-    const ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => {
-      clearInterval(ticker);
-    };
-  }, [text]);
-
-  const tick = () => {
-    const i = loopNum % toRotate.length;
-    const fullText = toRotate[i];
-    const updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setDelta(period);
-    } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setDelta(500);
-    }
-  };
-
+  
 
     return(
       <>
-        <h1>My Projects</h1>
+      <center>
+        <h1 className='my-projects-header' id='my-projects-header'>My Projects</h1>
+
+        <div className='projects' id='projects'>
+
+        </div>
+      </center>
       </>
       
     )
